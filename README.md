bbda0f63110d4e677fbeeb92a21945e5eea826fd
lc6464 <64722907+lc6464@users.noreply.github.com>
2024-05-08 20:12:17 +0800

    更新多种模式

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # TencentCloudVPCTemplateUpdater
-腾讯云私有网络参数模板 IPv4 地址自动更新器，支持服务，支持自动安装为 Windows 服务。
+腾讯云私有网络参数模板 IP 地址自动更新器，支持服务，支持自动安装为 Windows 服务。
+
+## app settings
+| Key                | Value                      |
+| ------------------ | -------------------------- |
+| Secret:Id          | TencentCloud SecretId      |
+| Secret:Key         | TencentCloud SecretKey     |
+| Region             | TencentCloud Region        |
+| TemplateIds:Mode   | Templates updating mode    |
+| TemplateIds:Single | Template Id, single mode   |
+| TemplateIds:V4     | TemplateV4 Id, multi mode  |
+| TemplateIds:V6     | TemplateV6 Id, multi mode  |
+
+## TemplateIds:Mode
+| Mode | Description                                                 |
+| ---- | ----------------------------------------------------------- |
+| 0    | Single template, auto version.                              |
+| 1    | Multiple templates, auto version, keep the other.           |
+| 2    | Multiple templates, auto version, set the other loopback.   |
+| 3    | Multiple templates, IPv6 only, keep IPv4.                   |
+| 4    | Multiple templates, IPv6 only, set IPv4 loopback.           |
+| 5    | Multiple templates, IPv4 only, keep IPv6.                   |
+| 6    | Multiple templates, IPv4 only, set IPv6 loopback.           |
+| -1   | Disable updater.                                            |
--- WindowsBackgroundService.cs
+++ WindowsBackgroundService.cs
@@ -19,6 +19,17 @@ public sealed class WindowsBackgroundService(ILogger<WindowsBackgroundService> l
 		logger.LogDebug("服务已启动。");
 
 		try {
+			var mode = configuration.GetValue<int>("TemplateIds:Mode");
+			if (!new int[] { -1, 0, 1, 2, 3, 4, 5, 6 }.Contains(mode)) {
+				logger.LogError("服务配置的运行模式有误，请修改配置文件后再次运行！");
+				Environment.Exit(-1); // skipcq: CS-W1005 模式有误，直接退出
+			}
+
+			if (mode == -1) {
+				logger.LogError("服务在配置文件中被禁用，请修改配置文件后再次运行！");
+				Environment.Exit(-1); // skipcq: CS-W1005 服务被禁用，直接退出
+			}
+
 			Credential credential = new() {
 				SecretId = configuration.GetValue<string>("Secret:Id"),
 				SecretKey = configuration.GetValue<string>("Secret:Key")
@@ -26,21 +37,35 @@ public sealed class WindowsBackgroundService(ILogger<WindowsBackgroundService> l
 
 			VpcClient client = new(credential, configuration.GetValue<string>("Region"));
 
-			ModifyAddressTemplateAttributeRequest request = new() {
-				AddressTemplateId = configuration.GetValue<string>("TemplateId")
+			ModifyAddressTemplateAttributeRequest requestV4 = new() {
+				AddressTemplateId = configuration.GetValue<string>("TemplateIds:V4")
+			}, requestV6 = new() {
+				AddressTemplateId = configuration.GetValue<string>("TemplateIds:V6")
+			}, requestSingle = new() {
+				AddressTemplateId = configuration.GetValue<string>("TemplateIds:Single")
 			};
 
 			while (!stoppingToken.IsCancellationRequested) {
 				try {
-					string? address = null;
+					string? address = null,
+						family = null;
 
 					try {
 						var addressResult = await httpClient.GetFromJsonAsync<IP>("", stoppingToken).ConfigureAwait(false);
+						family = addressResult.Family;
 
-						if (addressResult.Family != "IPv4") {
-							logger.LogInformation("检测到不是使用 IPv4 连接，可能是使用了 IPv6，无需更新 IPv4 地址，已跳过，三十分钟后继续。");
+						if (family == "IPv4") {
+							requestV4.Addresses = [addressResult.Address];
+							requestV6.Addresses = ["::1"];
+							requestSingle.Addresses = requestV4.Addresses;
+						} else if (family == "IPv6") {
+							requestV4.Addresses = ["127.0.0.1"];
+							requestV6.Addresses = [addressResult.Address];
+							requestSingle.Addresses = requestV6.Addresses;
+						} else {
+							logger.LogError("获取到的 IP 地址 {Address} 协议为 {Family}，目前暂不支持不支持，五分钟后重试。", addressResult.Address, family);
 
-							await Task.Delay(TimeSpan.FromMinutes(30), stoppingToken);
+							await Task.Delay(TimeSpan.FromMinutes(5), stoppingToken);
 
 							continue;
 						}
@@ -70,20 +95,82 @@ public sealed class WindowsBackgroundService(ILogger<WindowsBackgroundService> l
 						continue;
 					}
 
-					request.Addresses = [address];
+					ModifyAddressTemplateAttributeResponse? responseV4 = null,
+						responseV6 = null,
+						responseSingle = null;
 
-					var response = await client.ModifyAddressTemplateAttribute(request).ConfigureAwait(false);
+					switch (mode) {
+						case 0: // 单模板模式
+							responseSingle = await client.ModifyAddressTemplateAttribute(requestSingle).ConfigureAwait(false);
+							break;
+						case 1: // 双模板模式，只更新获取到的地址
+							if (family == "IPv4") {
+								responseV4 = await client.ModifyAddressTemplateAttribute(requestV4).ConfigureAwait(false);
+							} else {
+								responseV6 = await client.ModifyAddressTemplateAttribute(requestV6).ConfigureAwait(false);
+							}
+							break;
+						case 2: // 双模板模式，将未获取到的地址设置为环回地址
+							responseV4 = await client.ModifyAddressTemplateAttribute(requestV4).ConfigureAwait(false);
+							responseV6 = await client.ModifyAddressTemplateAttribute(requestV6).ConfigureAwait(false);
+							break;
+						case 3: // IPv6 only 模式，不修改 IPv4 地址；若接收到 IPv4 地址则跳过，十分钟后重试
+							if (family == "IPv4") {
+								logger.LogWarning("获取到的 IP 地址 {Address} 协议为 IPv4，目前设置了 IPv6 only 模式，十分钟后重试。", address);
+
+								await Task.Delay(TimeSpan.FromMinutes(10), stoppingToken);
+
+								continue;
+							}
+							responseV6 = await client.ModifyAddressTemplateAttribute(requestV6).ConfigureAwait(false);
+							break;
+						case 4: // IPv6 only 模式，将 IPv4 地址设为环回地址；若接收到 IPv4 地址则跳过，十分钟后重试
+							if (family == "IPv4") {
+								logger.LogWarning("获取到的 IP 地址 {Address} 协议为 IPv4，目前设置了 IPv6 only 模式，十分钟后重试。", address);
+
+								await Task.Delay(TimeSpan.FromMinutes(10), stoppingToken);
+
+								continue;
+							}
+							responseV4 = await client.ModifyAddressTemplateAttribute(requestV4).ConfigureAwait(false);
+							responseV6 = await client.ModifyAddressTemplateAttribute(requestV6).ConfigureAwait(false);
+							break;
+						case 5: // IPv4 only 模式，不修改 IPv6 地址；若接收到 IPv6 地址则跳过，十分钟后重试
+							if (family == "IPv6") {
+								logger.LogWarning("获取到的 IP 地址 {Address} 协议为 IPv6，目前设置了 IPv4 only 模式，十分钟后重试。", address);
+
+								await Task.Delay(TimeSpan.FromMinutes(10), stoppingToken);
+
+								continue;
+							}
+							responseV4 = await client.ModifyAddressTemplateAttribute(requestV4).ConfigureAwait(false);
+							break;
+						case 6: // IPv4 only 模式，将 IPv6 地址设为环回地址；若接收到 IPv6 地址则跳过，十分钟后重试
+							if (family == "IPv6") {
+								logger.LogWarning("获取到的 IP 地址 {Address} 协议为 IPv6，目前设置了 IPv4 only 模式，十分钟后重试。", address);
+
+								await Task.Delay(TimeSpan.FromMinutes(10), stoppingToken);
+
+								continue;
+							}
+							responseV4 = await client.ModifyAddressTemplateAttribute(requestV4).ConfigureAwait(false);
+							responseV6 = await client.ModifyAddressTemplateAttribute(requestV6).ConfigureAwait(false);
+							break;
+					}
 
 					lastAddress = address;
 
-					logger.LogInformation("IP 地址已更新为 {Address}，请求 ID 为 {RequestId}，十分钟后继续。", address, response.RequestId);
+					logger.LogInformation("Single 地址已更新为 {AddressSingle}，V6 地址已更新为 {AddressV6}，V4 地址已更新为 {AddressV4}，" +
+						"Single 请求 ID 为 {RequestIdSingle}，V6 请求 ID 为 {RequestIdV6}，V4 请求 ID 为 {RequestIdV4}，十分钟后继续。",
+						responseSingle is null ? "未更改" : requestSingle.Addresses[0], responseV6 is null ? "未更改" : requestV6.Addresses[0],
+						responseV4 is null ? "未更改" : requestV4.Addresses[0], responseSingle?.RequestId ?? "未请求",
+						responseV6?.RequestId ?? "未请求", responseV4?.RequestId ?? "未请求");
 				} catch (TencentCloudSDKException e) {
 					logger.LogError(e, "更新 IP 地址时出现腾讯云 SDK 异常，请求 ID 为 {RequestId}，五分钟后重试。", e.RequestId);
 
 					await Task.Delay(TimeSpan.FromMinutes(5), stoppingToken);
 
 					continue;
-
 				} catch (Exception e) {
 					logger.LogError(e, "更新 IP 地址时出现异常，五分钟后重试。");
 
